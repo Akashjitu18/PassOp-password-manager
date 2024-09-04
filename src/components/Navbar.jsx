37723cd163e199aacbb5b981efbdfe58b822c3fb
Akashjitu18 <akashjitu18@gmail.com>
2024-09-05 04:52:05 +0530

    added my github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                     <span>Pass</span>
                     <span className='text-green-500'>OP/&gt;</span>
                 </div>
-{/* 
+                {/* 
                 <ul>
                     <li className='flex gap-4 '>
                         <a className='hover:font-bold' href='/'>Home</a>
@@ -19,12 +19,13 @@ const Navbar = () => {
                     </li>
                 </ul> */}
 
+                <a href="https://github.com/Akashjitu18" target='_blank'>
+                    <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
+                        <img className='invert  w-10 p-1' src="icons/github.svg" alt="github logo" />
+                        <span className='font-bold px-2'>GitHub</span>
 
-                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
-                    <img className='invert  w-10 p-1' src="icons/github.svg" alt="github logo" />
-                    <span className='font-bold px-2'>GitHub</span>
-                    
-                </button>
+                    </button>
+                </a>
             </div>
         </nav>
     )
